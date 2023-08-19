b1d99a32f6f3535fd84e4e046f2539a82d4ac26a
gvkth <vokiencuong@gmail.com>
2023-08-20 00:19:17 +0700

    Thêm lớp service trung gian để tăng độ linh hoạt

--- src/a/b/c/controllers/CustomerController.java
+++ src/a/b/c/controllers/CustomerController.java
@@ -11,21 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import a.b.c.dao.CustomerDAO;
 import a.b.c.entity.Customer;
+import a.b.c.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
 	
-	//need to inject the customer dao
+	//need to inject the customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
 		
 		//get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		
 		//add the customers to the model
---
+++ src/a/b/c/service/CustomerService.java
@@ -0,0 +1,10 @@
+package a.b.c.service;
+
+import java.util.List;
+
+import a.b.c.entity.Customer;
+
+public interface CustomerService {
+	public List<Customer> getCustomers();
+
+}
